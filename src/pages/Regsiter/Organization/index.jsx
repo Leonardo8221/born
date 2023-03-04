348cab0c3e744544de2ad8b40f366b337808350f
Leonardo8221 <r.leonardo0802@gmail.com>
2023-03-04 18:05:37 +0100

    delete unused

--- src/pages/Regsiter/Organization/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledOrganization } from "./organization";
-
-const Organization = () => {
-  return <StyledOrganization></StyledOrganization>;
-};
-
-export default Organization;
--- src/pages/Regsiter/Organization/organization.js
+++
@@ -1,3 +0,0 @@
-export const StyledOrganization = ({ children, ...props }) => {
-  return <div {...props}>{children}</div>;
-};
