35774b7d6b018d419a43e8f14a774c464d74c0cc
Leonardo8221 <r.leonardo0802@gmail.com>
2023-03-09 09:56:01 +0200

    add tabs for marketing, fix ui of tabs component

--- src/pages/organization/[id]/manage/marketing.tsx
+++ src/pages/organization/[id]/manage/marketing.tsx
@@ -1,3 +1,4 @@
+import Tabs from "@/components/molecules/Tab/Tabs";
 import WrapperMarketing from "@/components/page-components/marketing/WrapperMarketing";
 import { useState } from "react";
 
@@ -5,6 +6,32 @@ const MarketingPage = () => {
 	const [currentIndexItemMenu, setCurrentIndexItemMenu] = useState(0);
 	const changeCurrentInexItemMenu = (index: number) =>
 		setCurrentIndexItemMenu(index);
+
+	const profileTabs = [
+		{
+			id: 1,
+			label: "Showcase",
+			content: <p>showcase</p>,
+		},
+		{
+			id: 2,
+			label: "Marketing",
+			content: <p>Marketing</p>,
+		},
+	];
+	const orderingTabs = [
+		{
+			id: 1,
+			label: "Terms and conditions",
+			content: <p>Terms and conditions</p>,
+		},
+		{
+			id: 2,
+			label: "Carried currencies",
+			content: <p>Carried currencies</p>,
+		},
+	];
+	
 	return (
 		<WrapperMarketing
 			currentIndexItem={currentIndexItemMenu}
@@ -12,7 +39,8 @@ const MarketingPage = () => {
 			hrefBack="/"
 			hrefClose="/"
 		>
-			<h1>Marketing page is under construction!</h1>
+			{currentIndexItemMenu === 0 && <Tabs tabs={profileTabs} />}
+			{currentIndexItemMenu === 1 && <Tabs tabs={orderingTabs} />}
 		</WrapperMarketing>
 	);
 };
