508a21ae274af18c6a0868cb67c7cc4de4bc2f66
Leonardo8221 <r.leonardo0802@gmail.com>
2023-03-30 12:53:09 +0200

    update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,10 +7,10 @@ export const authOptions = {
     providers: [
         KeycloakProvider({
             clientId: process.env.NEXT_PUBLIC_CLIENT_ID || "born-ui",
-            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET || "bqNTOGPXaivza7elslad4nZNIAgWlB41",
-            issuer: process.env.NEXT_PUBLIC_CLIENT_ISSUER || "https://alkeon-dev-sso-dot-master-works-375211.ew.r.appspot.com/auth/realms/Born",
-            accessTokenUrl: process.env.NEXT_ACCESS_TOKEN_URL || 'https://alkeon-dev-sso-dot-master-works-375211.ew.r.appspot.com/auth/realms/Born/protocol/openid-connect/token',
-            authorizationUrl: process.env.NEXT_AUTH_URL || 'https://alkeon-dev-sso-dot-master-works-375211.ew.r.appspot.com/auth/realms/Born/protocol/openid-connect/auth'
+            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET || "VnIKn4sSk56gLL7BhEfLUvPU7RnXI59x",
+            issuer: process.env.NEXT_PUBLIC_CLIENT_ISSUER || "https://alkeon-dev-sso-dot-master-works-375211.ew.r.appspot.com/auth/realms/BORN-STAGING",
+            accessTokenUrl: process.env.NEXT_ACCESS_TOKEN_URL || 'https://alkeon-dev-sso-dot-master-works-375211.ew.r.appspot.com/auth/realms/BORN-STAGING/protocol/openid-connect/token',
+            authorizationUrl: process.env.NEXT_AUTHORIZATION_URL || 'https://alkeon-dev-sso-dot-master-works-375211.ew.r.appspot.com/auth/realms/BORN-STAGING/protocol/openid-connect/auth'
         }),
         // ...add more providers here
     ],
