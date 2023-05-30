a3a15fc504bce67314c4c68fce776601114c8ae6
Leonardo8221 <r.leonardo0802@gmail.com>
2023-05-30 14:38:25 +0500

    adding changes

--- src/components/organisms/Tables/Product/OrderListTable/index.tsx
+++ src/components/organisms/Tables/Product/OrderListTable/index.tsx
@@ -74,7 +74,7 @@ const OrderListTable: FC<OrderDetails> = ({
             </div>
             <span
               className={clsx(
-                'text-shades-black tracking-[0.06em] text-center print:w-[60px]',
+                'text-shades-black tracking-[0.06em] text-center print:w-[60px] break-all',
                 fonts.text.md
               )}
             >
